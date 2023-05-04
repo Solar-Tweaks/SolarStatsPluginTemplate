9f0ce61967d17ccb8a96bda841936210c772ddaf
TBHGodPro <84408861+TBHGodPro@users.noreply.github.com>
2023-05-03 20:34:16 -0500

    feat: 🌟 better typings
    * typings
    
    * SolarStats link
    
    * Complete typings
    
    * Update package.json
    
    * Update index.js
    
    * Fix gitmodules
    
    * Fix submodule url

--- solarstats.d.ts
+++ solarstats.d.ts
@@ -1,40 +1,58 @@
-interface Package {
-  /**
-   * Defines the name of your plugin, this variable takes
-   * the value of the `displayName` key (wich is inside the
-   * `solarstats` key).
-   *
-   * If the `displayName` key isn't specified, the name
-   * of the package is used by default (`name` key).
-   *
-   * @example Here the value of this variable will be `Test`.
-   * File: `package.json`
-   * ```json
-   * {
-   *  "name": "test",
-   *  [...]
-   *  "solarstats": {
-   *    "displayName": "Test"
-   *  }
-   * }
-   * ```
-   */
-  name: string;
-  /**
-   * Defines the description of your plugin, this variable
-   * takes the value of the `description` key (`package.json`)
-   */
-  description: string;
-  /**
-   * Defines the version of your plugin, this variable
-   * takes the value of the `version` key (`package.json`)
-   */
-  version: string;
-  /**
-   * Defines the author of your plugin, this variable
-   * takes the value of the `author` key (`package.json`)
-   */
-  author: string;
+import Command from './SolarStats/src/Classes/Command';
+import PlayerModule from './SolarStats/src/player/PlayerModule';
+import { Axios } from 'axios';
+import LoggerClass from './SolarStats/src/Classes/Logger';
+import { PluginInfo } from './SolarStats/src/utils/plugins';
+import Inventory from './SolarStats/src/Classes/Inventory';
+import { InventoryType } from './SolarStats/src/Types';
+import Item from './SolarStats/src/Classes/Item';
+import { getConfig, getConfigAsync } from './SolarStats/src/utils/config';
+import Player from './SolarStats/src/player/Player';
+import { StringComponentBuilder } from '@minecraft-js/chat';
+
+export {};
+
+declare global {
+  var toolbox: Toolbox;
+  var player: Player;
+
+  var __package: PluginInfo;
+  var __cwd: string;
+  var __plugins: string;
+
+  var Logger: LoggerClass;
+
+  var registerPlugin: (plugin: PluginInfo) => void;
+  var registerCommand: (command: Command) => void;
+  var registerPlayerModule: (module: PlayerModule) => void;
+  var requireModule: (module: string) => any;
+
+  var dirFetch: typeof fetch;
+  var axios: Axios;
 }
 
-declare const __package: Package;
+interface Toolbox {
+  Command: typeof Command;
+  PlayerModule: typeof PlayerModule;
+  Inventory: typeof Inventory;
+  InventoryType: InventoryType;
+  Item: typeof Item;
+  Message: typeof StringComponentBuilder;
+  getConfig: typeof getConfigAsync;
+  getConfigSync: typeof getConfig;
+  parseNBTData: (
+    data: Buffer | string,
+    nbtType?: 'big' | 'little' | 'littleVarint'
+  ) => Promise<{
+    parsed: {
+      type: 'compound';
+      value: any;
+      name: string;
+    };
+    type: 'big' | 'little' | 'littleVarint';
+    metadata: {
+      buffer: Buffer;
+      size: number;
+    };
+  }>;
+}
